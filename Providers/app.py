e9372c276b870ece6f1c4046cf7c7864e2a1b90e
natalicot <natalicot@gmail.com>
2020-07-28 14:44:05 +0300

    newer

--- Providers/app.py
+++ Providers/app.py
@@ -93,6 +93,6 @@ def get_health():
 # def get_tasks():
 #     return jsonify({'tasks': tasks})
 
-
+app.run(host='0.0.0.0', port="5000")
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port="5000")
+    app.run(debug=True)
