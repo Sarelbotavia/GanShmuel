e75d7da844dc6bafa1882fa329b72dc9af0fb142
sol <bl.shlomi@gmail.com>
2020-07-29 22:04:37 +0300

    improving the image dockerfile and addign requirements.txt

--- Weight/alp-sql.sh
+++ Weight/alp-sql.sh
@@ -3,8 +3,12 @@ set -e
 echo "apk add --no-cache --virtual .build-deps gcc libc-dev linux-headers mariadb-dev python3-dev"
 apk add --no-cache --virtual .build-deps gcc libc-dev linux-headers
 echo "pip install mysqlclient"
-pip install mysqlclient flask flask-mysqldb
+pip install pipreqs
+pipreqs /app
+pip install -r /app/requirements.txt
+#pip install mysqlclient flask flask-mysqldb pipreqs
 echo "apk del .build-deps"
 apk del .build-deps
+pip unistall pipreqs
 #apk add --no-cache mariadb-client-libs
 #apk add --no-cache mariadb-client
