534da90103beb87b251a14b92817fc7d7b663c2a
sol <bl.shlomi@gmail.com>
2020-07-30 03:01:57 +0300

    Dockerfile

--- Providers/alp-sql.sh
+++ Providers/alp-sql.sh
@@ -3,7 +3,7 @@ set -e
 echo "apk add --no-cache --virtual .build-deps gcc libc-dev linux-headers mariadb-dev python3-dev"
 apk add --no-cache --virtual .build-deps gcc libc-dev linux-headers
 echo "pip install mysqlclient"
-sed '/^pandas.*$/d' Providers/requirements.txt 
+sed '/^pandas.*$/d' requirements.txt 
 pip install -r requirements.txt
 echo "apk del .build-deps"
 apk del .build-deps
