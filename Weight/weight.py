c639f5ecb45c59e08b006b1c3de917ae50098f63
Shmuel Arditi <externalrs@gmail.com>
2020-07-31 03:52:04 +0300

    fix pipline

--- Weight/weight.py
+++ Weight/weight.py
@@ -427,7 +427,7 @@ def get_health():
         return "RUNNING"
 
 
-#app.run(debug=True,host='0.0.0.0', port=5000)
+app.run(debug=True,host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     app.run()
